fd6dc5abc8ef7b734490de685e0dfe2c37cf8c80
aakashdeveloper <ahanda205@gmail.com>
2023-07-04 07:33:11 +0530

    code

--- apiWithui.js
+++ apiWithui.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const {MongoClient} = require('mongodb');
 const Mongo = require('mongodb');
-const url = "mongodb+srv://local:test12345@cluster0.f8vmc.mongodb.net/augintern?retryWrites=true&w=majority";
+const url = "";
 const client = new MongoClient(url);
 
 app.use(express.static(__dirname + '/public'))
